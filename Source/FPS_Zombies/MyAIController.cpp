355e9bccf1ed7842d186d3538dcc2e31680ab345
Manvendra Singh Rawat <manvendrasingh.r11@gmail.com>
2024-02-22 21:52:59 +0530

    cleanup

---
+++ Source/FPS_Zombies/MyAIController.cpp
@@ -0,0 +1,1 @@
+#include "MyAIController.h"
---
+++ Source/FPS_Zombies/MyAIController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+UCLASS()
+class FPS_ZOMBIES_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
